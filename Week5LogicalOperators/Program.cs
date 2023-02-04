92295c073fdc1a03854aa923b9e3f0f62380e311
RanRai <ranno.raidla@tktk.ee>
2023-02-04 12:46:26 +0200

    add Week5LogicalOperators code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿//AND - && range tingimuste kontroll
+
+//true && true && --> true
+//true && false --> false
+//false && true --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || false --> false
